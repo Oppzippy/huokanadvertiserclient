ac7c97be152a64410e28256931a682f8097e626e
Kyle Scheuing <kyle@kylescheuing.com>
2022-01-08 22:24:50 -0500

    Add PanelSwitcher abstraction

--- huokanadvertiserclient/gui/MainPanel.py
+++ huokanadvertiserclient/gui/MainPanel.py
@@ -4,33 +4,22 @@ import wx
 from huokanadvertiserclient.config.Configuration import Configuration
 from huokanadvertiserclient.gui.Login import Login
 from huokanadvertiserclient.gui.Modules import Modules
+from huokanadvertiserclient.gui.framework.PanelSwitcher import PanelSwitcher
 from huokanadvertiserclient.gui.framework.ReactivePanel import ReactivePanel
 
 
-class MainPanel(ReactivePanel):
+class MainPanel(ReactivePanel, PanelSwitcher):
     def __init__(self, parent, config: Configuration):
         super().__init__(parent)
         self._config = config
         self._login: Union[Login, None] = None
         self._modules: Union[Modules, None] = None
-        self._sizer = wx.BoxSizer(wx.VERTICAL)
-        self.SetSizer(self._sizer)
         self.bind_observable(config.api_key, self._on_api_key_change)
 
     def _on_api_key_change(self, api_key: Union[str, None]):
         if self._login is not None and api_key is None:
             return
-
-        self.DestroyChildren()
-        self._login = None
-        self._modules = None
-        self._sizer.Clear()
-
         if api_key is None:
-            self._login = Login(self, self._config)
-            self._sizer.Add(self._login, proportion=1, flag=wx.EXPAND)
+            self.set_panel(Login(self, self._config))
         else:
-            self._modules = Modules(self, self._config)
-            self._sizer.Add(self._modules, proportion=1, flag=wx.EXPAND)
-        self.Layout()
-        self.Refresh()
+            self.set_panel(Modules(self, self._config))
---
+++ huokanadvertiserclient/gui/framework/PanelSwitcher.py
@@ -0,0 +1,20 @@
+from typing import Union
+import wx
+
+
+class PanelSwitcher(wx.Panel):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self._active_panel: Union[wx.Panel, None] = None
+        self._sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self._sizer)
+
+    # panel must be a child of self
+    def set_panel(self, panel: wx.Panel):
+        if self._active_panel is not None:
+            self._active_panel.Destroy()
+        self._sizer.Clear()
+        self._active_panel = panel
+        self._sizer.Add(self._active_panel, proportion=1, flag=wx.EXPAND)
+        self.Layout()
+        self.Refresh()
