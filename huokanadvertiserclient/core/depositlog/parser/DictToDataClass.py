4da244bf954dc6f46ce6bbee44ce537b534022e1
Kyle Scheuing <kyle@kylescheuing.com>
2022-01-26 04:21:01 -0500

    Add approximate deposit timestamp to deposit log parser

--- huokanadvertiserclient/core/depositlog/parser/DictToDataClass.py
+++ huokanadvertiserclient/core/depositlog/parser/DictToDataClass.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from typing import Dict, List
 
 from attr import dataclass
@@ -10,6 +11,7 @@ class DepositLogEntry:
     character_realm: str
     guild_bank_copper: int
     deposit_in_copper: int
+    approximate_deposit_timestamp: datetime
 
 
 @dataclass
@@ -21,20 +23,36 @@ class DepositLog:
 
 
 def parse_deposit_log_dict(raw_logs: List[Dict]) -> List[DepositLog]:
-    return [
-        DepositLog(
-            log=[
-                DepositLogEntry(
-                    character_name=entry["characterName"],
-                    character_realm=entry["characterRealm"],
-                    guild_bank_copper=entry["guildBankCopper"],
-                    deposit_in_copper=entry["depositInCopper"],
-                )
-                for entry in log["log"]
-            ],
-            guild_name=log["guildName"],
-            guild_realm=log["guildRealm"],
-            captured_at=datetime.fromtimestamp(log["capturedAt"]),
-        )
-        for log in raw_logs
-    ]
+    return [_parse_deposit_log(log) for log in raw_logs]
+
+
+def _parse_deposit_log(log: dict) -> DepositLog:
+    captured_at = datetime.fromtimestamp(log["capturedAt"])
+    return DepositLog(
+        log=[
+            DepositLogEntry(
+                character_name=entry["characterName"],
+                character_realm=entry["characterRealm"],
+                guild_bank_copper=entry["guildBankCopper"],
+                deposit_in_copper=entry["depositInCopper"],
+                approximate_deposit_timestamp=_time_elapsed_to_datetime(
+                    entry["timeElapsed"],
+                    captured_at,
+                ),
+            )
+            for entry in log["log"]
+        ],
+        guild_name=log["guildName"],
+        guild_realm=log["guildRealm"],
+        captured_at=captured_at,
+    )
+
+
+def _time_elapsed_to_datetime(time_elapsed: Dict, since: datetime) -> datetime:
+    delta = relativedelta(
+        years=time_elapsed["years"],
+        months=time_elapsed["months"],
+        days=time_elapsed["days"],
+        hours=time_elapsed["hours"],
+    )
+    return since + delta
